cfe0efef74d7ed473ea05e4a22c164468edb6e5f
Shane-HXY <gmlshane@gmail.com>
2016-04-11 17:57:02 +0800

    step1

---
+++ CoolWeatherDemo/app/src/main/java/com/example/huangxiangyu/coolweatherdemo/model/Province.java
@@ -0,0 +1,35 @@
+package com.example.huangxiangyu.coolweatherdemo.model;
+
+/**
+ * Created by huangxiangyu on 16/4/11.
+ * In CoolWeatherDemo
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
